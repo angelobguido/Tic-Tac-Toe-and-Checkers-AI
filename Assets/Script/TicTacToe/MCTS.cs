c755a727bee8c9347c57562b39fb87ee8b4eb463
angelobguido <angelobguido@gmail.com>
2023-02-27 18:01:48 -0300

    Update values

--- Assets/Script/TicTacToe/MCTS.cs
+++ Assets/Script/TicTacToe/MCTS.cs
@@ -13,7 +13,7 @@ namespace TicTacToe
         private Game game;
         private PlayerType player;
         private Node root;
-        private static readonly int NumberOfIterations = 1000;
+        private static readonly int NumberOfIterations = 200;
         
         public MCTS(Game currentGame, PlayerType targetPlayer)
         {
